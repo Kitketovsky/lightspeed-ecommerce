7659cea7b23254e48393a489e91eabd1cdee3036
kitketovsky <kitketovsky@gmail.com>
2025-11-18 02:01:18 +0400

    compose home page and add sections wrapper

---
+++ src/components/StoreSection.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ heading: string }>()
+</script>
+
+<template>
+  <section class="mb-12">
+    <h2 class="text-3xl font-semibold mb-6 text-gray-700">{{ heading }}</h2>
+    <slot />
+  </section>
+</template>
